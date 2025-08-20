e2924ad62316a2791272faf9df5b3699c6a11a70
usmansafk <usman230@hotmail.co.uk>
2025-08-20 11:52:54 +0100

    variables and datatypes

--- 1.js
+++ 1.js
@@ -4,3 +4,28 @@ alert("Hello, World");
 console.log("Hello Console's World");
 console.error("X");
 console.warn("!!!");
+
+// Variables
+
+// var - globally scoped variables
+// let - can reassign values (ES6)
+// const - constant (ES6)
+// Always use const unless you know variable value will change
+
+let age = 27;
+age = 28;
+
+const finalAge = 28;
+
+// Data Types: String, Numbers, Boolean, null, undefined
+
+const name = "Usman Sajid";
+const myAge = 28;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof x); // object
+console.log(typeof z); // undefined
